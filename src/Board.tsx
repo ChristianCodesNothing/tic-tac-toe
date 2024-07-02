74e18e047c1fa51e04c01ddbbcf4294d9c118d30
ChristianCodesNothing <christiand.briggs@gmail.com>
2024-07-02 17:42:45 -0400

    Main gameplay loop functional

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,19 +1,21 @@
+import { useState } from "react"
 import { Square } from "./Square"
 
 interface Props {}
 
 export const Board: React.FC<Props> = ({}) => {
+  const [playerTurn, setPlayerTurn] = useState<"x" | "o">("x")
   return (
     <div className="grid grid-cols-3  grid-rows-3 h-96 w-96">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
+      <Square playerTurn={playerTurn} setPlayerTurn={setPlayerTurn} />
     </div>
   )
 }
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,5 +1,21 @@
-interface Props {}
+import { useState } from "react"
 
-export const Square: React.FC<Props> = ({}) => {
-    return <div className="h-full w-full border border-black"> </div>
+interface Props {
+  playerTurn: "x" | "o"
+  setPlayerTurn: React.Dispatch<React.SetStateAction<"x" | "o">>
+}
+
+export const Square: React.FC<Props> = ({ playerTurn, setPlayerTurn }) => {
+  const [tileValue, setTileValue] = useState<"" | "x" | "o">("")
+  return (
+    <button
+      onClick={() => {
+        setTileValue(playerTurn)
+        setPlayerTurn(playerTurn === "x" ? "o" : "x")
+      }}
+      className="h-full w-full border border-black"
+    >
+      {tileValue}
+    </button>
+  )
 }
